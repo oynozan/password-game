9fa65c1e48be8bb56c4bbea2c4920fb20c361126
Ozan Andaç <oynozan@hotmail.com>
2025-06-29 20:11:23 +0200

    playing confetti animation when user completes all tasks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import type React from "react";
+import { useEffect, type ChangeEvent } from "react";
 
 import { Task } from "../lib/tasks/Task";
 import { usePasswordStore } from "../lib/store";
@@ -12,8 +12,13 @@ export default function HomePage() {
         usePasswordStore();
 
     const revealedTasks: Task[] = taskManager.getRevealedTasks(revealedCount);
+    const allTasks = taskManager.getTasks();
+    const allCompleted =
+        revealedCount === allTasks.length &&
+        revealedTasks.length === allTasks.length &&
+        revealedTasks.every(task => task.isValid);
 
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    const handleChange = (e: ChangeEvent<HTMLInputElement>): void => {
         setPassword(e.target.value);
         validateTasks();
 
@@ -32,10 +37,23 @@ export default function HomePage() {
         }
     };
 
+    useEffect(() => {
+        if (allCompleted) {
+            // Lazy loading confetti library
+            import("canvas-confetti").then(confetti => {
+                confetti.default({
+                    particleCount: 150,
+                    spread: 70,
+                    origin: { y: 0.6 },
+                });
+            });
+        }
+    }, [allCompleted]);
+
     return (
         <main className="text-white max-w-md mx-auto my-8">
             <h1 className="text-2xl font-bold text-center mb-2">The Password Game</h1>
-            <p className="text-center mb-4">Please log in, if you can :)</p>
+            <p className="text-center mb-4">Please log in, if you can :&#41;</p>
             <PasswordInput
                 value={password}
                 onChange={handleChange}
